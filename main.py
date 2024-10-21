52e79a7c252406c4342c0ce5a5a94781f09c7f9e
kzone159 <kzone159@gmail.com>
2024-10-21 10:52:56 -0400

    Even more changes

--- main.py
+++ main.py
@@ -2,21 +2,27 @@
 from news import *
 from OpenAi import *
 # from config import *
-# from trade import *
+from trade import *
 
 
 
+BuyTeslaFlag = True
+SellTeslaFlag = True
 
-
+TeslaNewsHeadline = ''
 
 if __name__ == '__main__':
     try:
-        newsData = newsHeadline("Tesla")
+        headline= "Tesla"
+        newsData = newsHeadline(headline)
         print(newsData[0])
+        TeslaNewsHeadline = newsData[0]
         AiPrompt = int(OpenAiPrompt(newsData[0]).content)
         
-        if(AiPrompt >=60): ## this is when we buy.
-            print("hi")
+        if (AiPrompt >=60 and SellTeslaFlag == True and headline): ## this is when we buy. ##TODO fix this so that it is for all the different stocks.
+            BuyTeslaFlag= False 
+            market_order_request_buy("TSLA",2.5)
+            
         elif (AiPrompt <= 40): ## this is when we sell
             print("bye")
         else:
